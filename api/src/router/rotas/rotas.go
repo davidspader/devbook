53b81e7be68ee773e127841e3e1f1754833d0c52
David Ambrozio <davidambroziodev@gmail.com>
2024-01-10 21:47:24 -0300

    update router

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -1,10 +1,26 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+// Rota reprenta todas as rotas da API
 type Rota struct {
 	URI                string
 	Metodo             string
 	funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+// Configurar coloca todas as rotas dentro do router
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.funcao).Methods(rota.Metodo)
+	}
+
+	return r
 }
