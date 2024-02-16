48998d76b51029682bbde3d1ced2c753c5f3f564
David Ambrozio <davidambroziodev@gmail.com>
2024-02-16 17:39:28 -0300

    finish webapp

--- webapp/go.mod
+++ webapp/go.mod
@@ -1,5 +1,9 @@
 module webapp
 
-go 1.21.4
+go 1.14
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/gorilla/securecookie v1.1.1
+	github.com/joho/godotenv v1.3.0
+)
--- webapp/src/router/rotas/rotas.go
+++ webapp/src/router/rotas/rotas.go
@@ -2,11 +2,12 @@ package rotas
 
 import (
 	"net/http"
+	"webapp/src/middlewares"
 
 	"github.com/gorilla/mux"
 )
 
-// Rota representa todas as rodas da aplicação Web
+// Rota representa todas as rotas da Aplicação Web
 type Rota struct {
 	URI                string
 	Metodo             string
@@ -18,9 +19,22 @@ type Rota struct {
 func Configurar(router *mux.Router) *mux.Router {
 	rotas := rotasLogin
 	rotas = append(rotas, rotasUsuarios...)
+	rotas = append(rotas, rotaPaginaPrincipal)
+	rotas = append(rotas, rotasPublicacoes...)
+	rotas = append(rotas, rotaLogout)
 
 	for _, rota := range rotas {
-		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+
+		if rota.RequerAutenticacao {
+			router.HandleFunc(rota.URI,
+				middlewares.Logger(middlewares.Autenticar(rota.Funcao)),
+			).Methods(rota.Metodo)
+
+		} else {
+			router.HandleFunc(rota.URI,
+				middlewares.Logger(rota.Funcao),
+			).Methods(rota.Metodo)
+		}
 	}
 
 	fileServer := http.FileServer(http.Dir("./assets/"))
